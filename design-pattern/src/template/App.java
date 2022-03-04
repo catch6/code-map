6e4b1d85244787ae7fd22eebeb4e6acebe9bbacb
Catch <catchlife6@163.com>
2022-03-04 16:57:26 +0800

    模板模式

---
+++ design-pattern/src/template/App.java
@@ -0,0 +1,14 @@
+package template;
+
+/**
+ * @author Catch
+ * @since 2022-03-04
+ */
+public class App {
+
+    public static void main(String[] args) {
+        SubClass subClass = new SubClass();
+        subClass.method();
+    }
+
+}
